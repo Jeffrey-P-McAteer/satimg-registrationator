970f1bd1e0a97af8d92f64fd912f5e88780b8033
Jeffrey McAteer <jeffrey@jmcateer.com>
2025-03-13 08:51:26 -0700

    faster renders, for some reason scene.clear() isnt doing much for us

--- generate_sat_imgs.py
+++ generate_sat_imgs.py
@@ -16,19 +16,28 @@ warnings.filterwarnings('ignore')
 import imageio
 
 from blendify import scene
-from blendify.materials import PrincipledBSDFMaterial
+from blendify.materials import PrincipledBSDFMaterial, PlasticMaterial
 from blendify.colors import UniformColors
 
 
 def render_one_shot(out_dir, file_prefix, img_w_px, img_h_px, camera_location_xyz, sun_angle_deg):
+
+  scene.clear()
+
   # Add light; https://virtualhumans.mpi-inf.mpg.de/blendify/api/blendify.lights.html#blendify.lights.collection.LightsCollection.add_sun
-  scene.lights.add_sun(strength=600, rotation_mode='look_at', rotation=(-2, -2, 0), angular_diameter=0.0093)
+  #scene.lights.add_sun(strength=600, rotation_mode='look_at', rotation=(-2, -2, 0), angular_diameter=0.0093)
+  scene.lights.add_point(strength=12000, shadow_soft_size=0.5, rotation_mode='look_at', rotation=(-2, -2, 0), translation=(-0.5, -0.5, 16))
+
   # Add camera
   scene.set_perspective_camera((img_w_px, img_h_px), fov_x=0.7, rotation_mode='look_at', rotation=(0, 0, 0), translation=camera_location_xyz)
+
   # Create material
   material = PrincipledBSDFMaterial(
     specular=0.1, roughness=2.6, sheen_tint=0.0, ior=5.0
   )
+  # material = PlasticMaterial(
+  #   specular=0.1, roughness=2.6, clearcoat_roughness=0.6
+  # )
 
   # Create color
   #color = UniformColors((0.0, 1.0, 0.0))
@@ -40,10 +49,12 @@ def render_one_shot(out_dir, file_prefix, img_w_px, img_h_px, camera_location_xy
 
   # "Ground"
   ground_color = UniformColors((207/255.0, 181/255.0, 144/255.0))
-  scene.renderables.add_cube_mesh(100.0, material, ground_color, translation=(0, 0, -100.0))
+  scene.renderables.add_cube_mesh(100.0, material, ground_color, translation=(0, 0, -50.0))
 
   # Render scene
-  scene.render(filepath=os.path.join(out_dir, f'{file_prefix}.png'))
+  out_png = os.path.join(out_dir, f'{file_prefix}.png')
+  #scene.render(filepath=os.path.join(out_dir, f'{file_prefix}.png'))
+  scene.preview(out_png)
 
 REPO_ROOT = os.path.dirname(__file__)
 imgs_folder = os.path.join(REPO_ROOT, 'build', 'imgs')
